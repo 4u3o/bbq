8dc2d21d8ef2b53dd87683b5a555259cf9106a49
Anton Goroshko <radjahair@gmail.com>
2020-12-08 14:27:01 +1000

    Add bootstrap_flash helper

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,5 @@ class Event < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :address, presence: true
   validates :datetime, presence: true
+  validates :user, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_many :events
+
+  validates :name, presence: true, length: { maximum: 35 }
+  validates :email,
+            presence: true, uniqueness: true,
+            length: { maximum: 255 },
+            format: URI::MailTo::EMAIL_REGEXP
 end
