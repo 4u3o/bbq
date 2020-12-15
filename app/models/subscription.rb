1545f5e609268192a0e1f14ffed2040cd4c7c682
Anton Goroshko <radjahair@gmail.com>
2020-12-15 16:33:47 +1000

    Add self_seubscribe validation to Subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -8,9 +8,12 @@ class Subscription < ApplicationRecord
             format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/,
             unless: -> { user.present? }
   validates :user,
-            uniqueness: {scope: :event_id}, if: -> { user.present? }
+            uniqueness: {scope: :event_id},
+            if: -> { user.present? }
   validates :user_email,
-            uniqueness: {scope: :event_id}, unless: -> { user.present? }
+            uniqueness: {scope: :event_id},
+            unless: -> { user.present? }
+  validate :cannot_self_subscribe
 
   def user_name
     return user.name if user.present?
@@ -20,5 +23,13 @@ class Subscription < ApplicationRecord
   def user_email
     return user.email if user.present?
     super
+  end
+
+  def cannot_self_subscribe
+    if user.present? && event.user == user || event.user.email == user_email
+      # Похоже здесь нельзя использовать локаль
+      # undefined method `t' for #<Subscription:...>
+      errors.add(:user_email, 'совпадает с Email автора события')
+    end
   end
 end
