3729bc828627d7b6bf6eb8cc6f449c61d4be8d2e
Anton Goroshko <radjahair@gmail.com>
2020-12-23 19:46:20 +1000

    Add event.photo lightbox gallery in events#show

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,7 +1,14 @@
 require("@rails/ujs").start()
+// require('jquery')
 
 import 'bootstrap/dist/js/bootstrap'
+import 'jquery/src/jquery'
+import 'ekko-lightbox/ekko-lightbox'
+
 import '../scripts/map'
+import '../scripts/lightbox'
+
 import '../styles/application'
 
 const images = require.context('../images', true)
+
