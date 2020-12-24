8f6ce5e70a71531ce1cbd9b4af151c6c894bd38e
Anton Goroshko <radjahair@gmail.com>
2020-12-24 16:53:40 +1000

    Change event.datetime picker to air-datepicker

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -3,6 +3,7 @@ require("@rails/ujs").start()
 import 'bootstrap/dist/js/bootstrap'
 import 'jquery/src/jquery'
 import 'ekko-lightbox/ekko-lightbox'
+import 'air-datepicker/dist/js/datepicker.min'
 
 import '../scripts/map'
 import '../scripts/lightbox'
